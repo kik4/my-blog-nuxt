7d60a590731797eeca00e37df990ab42a5d4fda4
kik4 <7163720+kik4@users.noreply.github.com>
2018-09-28 20:21:22 +0900

    update description

--- firebase/hosting/nuxt.config.js
+++ firebase/hosting/nuxt.config.js
@@ -8,7 +8,7 @@ module.exports = {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "kik4's page" },
+      { hid: "description", name: "description", content: "kik4's page: The engineer's portfolio, blog and others." },
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
