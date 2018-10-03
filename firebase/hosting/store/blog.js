f852bb4affee4dbcf22fbd8d7fd22fa8a7cc50c1
kik4 <7163720+kik4@users.noreply.github.com>
2018-10-03 20:18:49 +0900

    fix getArticles

--- firebase/hosting/store/blog.js
+++ firebase/hosting/store/blog.js
@@ -10,11 +10,9 @@ export const mutations = {
 
 export const actions = {
   async getArticles({ commit }) {
-    const response = await this.$axios
-      .$get("https://qiita.com/api/v2/users/kik4/items", { timeout: 5000 })
-      .catch(() => {})
-    if (response && response.data) {
-      commit("setArticles", response.data)
+    const data = await this.$axios.$get("https://qiita.com/api/v2/users/kik4/items", { timeout: 5000 }).catch(() => {})
+    if (data) {
+      commit("setArticles", data)
     } else {
       commit("setArticles", null)
     }
