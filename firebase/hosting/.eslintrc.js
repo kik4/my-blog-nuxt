5bfaebf4181ef25bb24ca191472df1055dafbad7
kik4 <7163720+kik4@users.noreply.github.com>
2018-10-15 18:16:40 +0900

    update eslintrc

--- firebase/hosting/.eslintrc.js
+++ firebase/hosting/.eslintrc.js
@@ -2,18 +2,28 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: "babel-eslint"
+    parser: "babel-eslint",
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    "plugin:vue/recommended"
+    "plugin:vue/recommended",
+    "plugin:prettier/recommended",
   ],
   // required to lint *.vue files
   plugins: ["vue"],
   // add your custom rules here
-  rules: {}
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        semi: false,
+        printWidth: 120,
+        trailingComma: "all",
+      },
+    ],
+  },
 }
